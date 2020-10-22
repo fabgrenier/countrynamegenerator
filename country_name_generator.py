566c45b0add6f422caeff2e559cf8b4225e5e221
Fabien <fabien@rtone.fr>
2020-10-22 22:55:23 +0200

    init script

---
+++ country_name_generator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import re
+import random
+
+countries = "Afghanistan", "Albanie", "Antarctique", "Algérie", "Samoa Américaines", "Andorre", "Angola", "Antigua-et-Barbuda", "Azerbaïdjan", "Argentine", "Australie", "Autriche", "Bahamas", "Bahreïn", "Bangladesh", "Arménie", "Barbade", "Belgique", "Bermudes", "Bhoutan", "Bolivie", "Bosnie-Herzégovine", "Botswana", "Île Bouvet", "Brésil", "Belize", "Territoire Britannique de l'Océan Indien", "Îles Salomon", "Îles Vierges Britanniques", "Brunéi Darussalam", "Bulgarie", "Myanmar", "Burundi", "Bélarus", "Cambodge", "Cameroun", "Canada", "Cap-vert", "Îles Caïmanes", "République Centrafricaine", "Sri Lanka", "Tchad", "Chili", "Chine", "Taïwan", "Île Christmas", "Îles Cocos (Keeling)", "Colombie", "Comores", "Mayotte", "République du Congo", "République Démocratique du Congo", "Îles Cook", "Costa Rica", "Croatie", "Cuba", "Chypre", "République Tchèque", "Bénin", "Danemark", "Dominique", "République Dominicaine", "équateur", "El Salvador", "Guinée équatoriale", "éthiopie", "érythrée", "Estonie", "Îles Féroé", "Îles (malvinas) Falkland", "Géorgie du Sud et les Îles Sandwich du Sud", "Fidji", "Finlande", "Îles Åland", "France", "Guyane Française", "Polynésie Française", "Terres Australes Françaises", "Djibouti", "Gabon", "Géorgie", "Gambie", "Territoire Palestinien Occupé", "Allemagne", "Ghana", "Gibraltar", "Kiribati", "Grèce", "Groenland", "Grenade", "Guadeloupe", "Guam", "Guatemala", "Guinée", "Guyana", "Haïti", "Îles Heard et Mcdonald", "Saint-Siège (état de la Cité du Vatican)", "Honduras", "Hong-Kong", "Hongrie", "Islande", "Inde", "Indonésie", "République Islamique d'Iran", "Iraq", "Irlande", "Israël", "Italie", "Côte d'Ivoire", "Jamaïque", "Japon", "Kazakhstan", "Jordanie", "Kenya", "République Populaire Démocratique de Corée", "République de Corée", "Koweït", "Kirghizistan", "République Démocratique Populaire Lao", "Liban", "Lesotho", "Lettonie", "Libéria", "Jamahiriya Arabe Libyenne", "Liechtenstein", "Lituanie", "Luxembourg", "Macao", "Madagascar", "Malawi", "Malaisie", "Maldives", "Mali", "Malte", "Martinique", "Mauritanie", "Maurice", "Mexique", "Monaco", "Mongolie", "République de Moldova", "Montserrat", "Maroc", "Mozambique", "Oman", "Namibie", "Nauru", "Népal", "Pays-Bas", "Antilles Néerlandaises", "Aruba", "Nouvelle-Calédonie", "Vanuatu", "Nouvelle-Zélande", "Nicaragua", "Niger", "Nigéria", "Niué", "Île Norfolk", "Norvège", "Îles Mariannes du Nord", "Îles Mineures éloignées des états-Unis", "états Fédérés de Micronésie", "Îles Marshall", "Palaos", "Pakistan", "Panama", "Papouasie-Nouvelle-Guinée", "Paraguay", "Pérou", "Philippines", "Pitcairn", "Pologne", "Portugal", "Guinée-Bissau", "Timor-Leste", "Porto Rico", "Qatar", "Réunion", "Roumanie", "Fédération de Russie", "Rwanda", "Sainte-Hélène", "Saint-Kitts-et-Nevis", "Anguilla", "Sainte-Lucie", "Saint-Pierre-et-Miquelon", "Saint-Vincent-et-les Grenadines", "Saint-Marin", "Sao Tomé-et-Principe", "Arabie Saoudite", "Sénégal", "Seychelles", "Sierra Leone", "Singapour", "Slovaquie", "Viet Nam", "Slovénie", "Somalie", "Afrique du Sud", "Zimbabwe", "Espagne", "Sahara Occidental", "Soudan", "Suriname", "Svalbard et Île Jan Mayen", "Swaziland", "Suède", "Suisse", "République Arabe Syrienne", "Tadjikistan", "Thaïlande", "Togo", "Tokelau", "Tonga", "Trinité-et-Tobago", "émirats Arabes Unis", "Tunisie", "Turquie", "Turkménistan", "Îles Turks et Caïques", "Tuvalu", "Ouganda", "Ukraine", "L'ex-République Yougoslave de Macédoine", "égypte", "Royaume-Uni", "Île de Man", "République-Unie de Tanzanie", "états-Unis", "Îles Vierges des états-Unis", "Burkina Faso", "Uruguay", "Ouzbékistan", "Venezuela", "Wallis et Futuna", "Samoa", "Yémen", "Serbie-et-Monténégro", "Zambie"
+
+morphemes = []
+ex = r'([^aeiou]*[aeiou]*)|[aeiou]*[^aeiou]*[aeiou]*'
+
+for root in countries:
+    while root != '':
+        end = re.match(ex, root).end()
+        morphemes.append(root[0:end])
+        root = root[end:]
+
+for i in range(10):
+    name = "{}{}".format(morphemes[random.randint(0, len(morphemes)-1)], morphemes[random.randint(0, len(morphemes)-1)])
+    name = re.sub(r'\W+', '', name)
+    print(name.title())
